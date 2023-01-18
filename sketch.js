43ff0d1dde5e625fd07592a3011293d5b62436c3
jvahle <48984317+webflowjvahle@users.noreply.github.com>
2023-01-18 08:16:02 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,81 +1,61 @@
-let navLinkSketch = document.querySelector("[data-sketch");
-navLinkSketch.addEventListener("click", function () {
-  console.log("navLinkSketch has been clicked");
-  setTimeout(function () {
-    const canvasSketchParent = document.querySelector(".c-sketch");
+let targetNodeSketch = document.querySelector(".c-main");
 
-    const sketch = Sketch.create({
-      container: document.getElementById("sketchable"),
-      width: canvasSketchParent.offsetWidth,
-      height: canvasSketchParent.offsetWidth,
-      fullscreen: false,
-      autoclear: false,
-      touchmove: function () {
-        if (this.dragging) {
-          var touch = this.touches[0];
-          this.lineWidth = 3;
-          this.beginPath();
-          this.moveTo(touch.ox, touch.oy);
-          this.lineTo(touch.x, touch.y);
-          this.strokeStyle = "#ef233c";
-          this.stroke();
-        }
-      },
-    });
+let configSketch = { childList: true };
 
-    document
-      .getElementById("sketchable")
-      .addEventListener("mousedown", function (event) {
-        sketch.dragging = true;
-      });
+let callbackSketch = function (mutationsList) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      console.log("A child node has been added or removed.");
+      if (document.querySelector("[data-sketch]")) {
+        console.log("navLinkSketch element is present on the page");
+        let navLinkSketch = document.querySelector("[data-sketch");
+        navLinkSketch.addEventListener("click", function () {
+          console.log("navLinkSketch has been clicked");
+          setTimeout(function () {
+            const canvasSketchParent = document.querySelector(".c-sketch");
+            const sketchable = document.getElementById("sketchable");
+            const sketch = Sketch.create({
+              container: sketchable,
+              width: canvasSketchParent.offsetWidth,
+              height: canvasSketchParent.offsetWidth,
+              fullscreen: false,
+              autoclear: false,
+            });
 
-    document
-      .getElementById("sketchable")
-      .addEventListener("mousemove", function (event) {
-        if (sketch.dragging) {
-          sketch.lineWidth = 3;
-          sketch.beginPath();
-          sketch.moveTo(event.offsetX, event.offsetY);
-          sketch.lineTo(event.offsetX, event.offsetY);
-          sketch.strokeStyle = "#ef233c";
-          sketch.stroke();
-        }
-      });
+            let isDrawing = false;
 
-    document
-      .getElementById("sketchable")
-      .addEventListener("mouseup", function (event) {
-        sketch.dragging = false;
-      });
+            sketchable.addEventListener("pointerdown", (event) => {
+              isDrawing = true;
+              sketch.beginPath();
+              sketch.moveTo(event.clientX, event.clientY);
+            });
 
-    document
-      .getElementById("sketchable")
-      .addEventListener("touchstart", function (event) {
-        sketch.dragging = true;
-      });
+            sketchable.addEventListener("pointermove", (event) => {
+              if (isDrawing) {
+                sketch.lineWidth = 3;
+                sketch.lineTo(event.clientX, event.clientY);
+                sketch.strokeStyle = "#ef233c";
+                sketch.stroke();
+              }
+            });
 
-    document
-      .getElementById("sketchable")
-      .addEventListener("touchmove", function (event) {
-        if (sketch.dragging) {
-          var touch = event.touches[0];
-          sketch.lineWidth = 3;
-          sketch.beginPath();
-          sketch.moveTo(touch.pageX, touch.pageY);
-          sketch.lineTo(touch.pageX, touch.pageY);
-          sketch.strokeStyle = "#ef233c";
-          sketch.stroke();
-        }
-      });
+            sketchable.addEventListener("pointerup", () => {
+              isDrawing = false;
+            });
 
-    document
-      .getElementById("sketchable")
-      .addEventListener("touchend", function (event) {
-        sketch.dragging = false;
-      });
+            document
+              .getElementById("btnClear")
+              .addEventListener("click", () => {
+                sketch.clear();
+              });
+          }, 400);
+        });
+      } else {
+        console.log("navLinkSketch element is not present on the page");
+      }
+    }
+  }
+};
 
-    document.getElementById("btnClear").addEventListener("click", function () {
-      sketch.clear();
-    });
-  }, 800);
-});
+let observerSketch = new MutationObserver(callbackSketch);
+observerSketch.observe(targetNodeSketch, configSketch);
